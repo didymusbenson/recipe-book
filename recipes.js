4d77422e47ab84b376854d3359663528aa47e7c9
Didymus <didymus.benson@gmail.com>
2018-01-10 08:13:32 -0700

    added recipe

--- recipes.js
+++ recipes.js
@@ -1,84 +1,153 @@
-var recipes =
-    [{
-        name:"Best Chocolate Chip Cookies",
-            time: {
-                prep: 20,
-                cook: 10,
-                total: 60
-            },
-            preheat: 350,
-            ingredients: [{
-                    amount: 1,
-                    type: 'dairy',
-                    measurement: 'cup',
-                    item: 'butter'
+var recipes = [{
+        name: "Best Chocolate Chip Cookies",
+        time: {
+            prep: 20,
+            cook: 10,
+            total: 60
         },
-                {
-                    amount: 1,
-                    type: 'dry',
-                    measurement: 'cup',
-                    item: 'white sugar'
+        preheat: 350,
+        ingredients: [{
+                amount: 1,
+                type: 'dairy',
+                measurement: 'cup',
+                item: 'butter'
         },
-                {
-                    amount: 1,
-                    type: 'dry',
-                    measurement: 'cup',
-                    item: 'packed brown sugar'
+            {
+                amount: 1,
+                type: 'dry',
+                measurement: 'cup',
+                item: 'white sugar'
         },
-                {
-                    amount: 2,
-                    type: 'liquid',
-                    measurement: '',
-                    item: 'eggs'
+            {
+                amount: 1,
+                type: 'dry',
+                measurement: 'cup',
+                item: 'packed brown sugar'
         },
-                {
-                    amount: 2,
-                    type: 'liquid',
-                    measurement: 'teaspoon',
-                    item: 'vanilla extract'
+            {
+                amount: 2,
+                type: 'liquid',
+                measurement: '',
+                item: 'eggs'
         },
-                {
-                    amount: 3,
-                    type: 'dry',
-                    measurement: 'cup',
-                    item: 'flour'
+            {
+                amount: 2,
+                type: 'liquid',
+                measurement: 'teaspoon',
+                item: 'vanilla extract'
         },
-                {
-                    amount: 1,
-                    type: 'dry',
-                    measurement: 'teaspoon',
-                    item: 'baking soda'
+            {
+                amount: 3,
+                type: 'dry',
+                measurement: 'cup',
+                item: 'flour'
         },
-                {
-                    amount: 2,
-                    type: 'liquid',
-                    measurement: 'teaspoon',
-                    item: 'hot water'
+            {
+                amount: 1,
+                type: 'dry',
+                measurement: 'teaspoon',
+                item: 'baking soda'
         },
-                {
-                    amount: .5,
-                    type: 'dry',
-                    measurement: 'teaspoon',
-                    item: 'salt'
+            {
+                amount: 2,
+                type: 'liquid',
+                measurement: 'teaspoon',
+                item: 'hot water'
         },
-                {
-                    amount: 2,
-                    type: 'dry',
-                    measurement: 'cup',
-                    item: 'chocolate chips'
+            {
+                amount: .5,
+                type: 'dry',
+                measurement: 'teaspoon',
+                item: 'salt'
         },
-                {
-                    amount: 1,
-                    type: 'optional',
-                    measurement: 'cup',
-                    item: 'chopped walnuts'
+            {
+                amount: 2,
+                type: 'dry',
+                measurement: 'cup',
+                item: 'chocolate chips'
+        },
+            {
+                amount: 1,
+                type: 'optional',
+                measurement: 'cup',
+                item: 'chopped walnuts'
         }
         ],
-            instructions: [
+        instructions: [
             "Preheat oven to 350 degrees F (175 degrees C).",
-            "Cream together the butter, white sugar, and brown sugar until smooth.","Beat in the eggs one at a time, then stir in the vanilla.", "Dissolve baking soda in hot water. Add to batter along with salt.","Stir in flour, chocolate chips, and nuts.","Drop by large spoonfuls onto ungreased or foil-lined pans.",
+            "Cream together the butter, white sugar, and brown sugar until smooth.", "Beat in the eggs one at a time, then stir in the vanilla.", "Dissolve baking soda in hot water. Add to batter along with salt.", "Stir in flour, chocolate chips, and nuts.", "Drop by large spoonfuls onto ungreased or foil-lined pans.",
             "Bake for about 10 minutes in the preheated oven, or until edges are nicely browned.",
+        ]
+    },
+    {
+        name: "Creamy Cheesy Chicken Spaghetti",
+        time: {
+            prep: 10,
+            cook: 25,
+            total: 45
+        },
+        preheat: 350,
+        ingredients: [{
+                amount: 8,
+                type: 'pasta',
+                measurement: 'oz',
+                item: 'Angel Hair'
+        },
+            {
+                amount: 2,
+                type: 'dry',
+                measurement: 'cup',
+                item: 'chopped, cooked chicken'
+        },
+                      {
+                amount: 2,
+                type: 'liquid',
+                measurement: 'cans',
+                item: 'cream of chicken soup'
+        },
+                      {
+                amount: 1,
+                type: 'liquid',
+                measurement: 'cup',
+                item: 'salsa (of your choice)'
+        },
+                      {
+                amount: 1,
+                type: 'liquid',
+                measurement: 'cup',
+                item: 'sour cream'
+        },
+                      {
+                amount: 2,
+                type: 'dry',
+                measurement: 'cup',
+                item: 'Mexican cheese (divided)'
+        },
+                      {
+                amount: 1,
+                type: 'dry',
+                measurement: 'tbsp',
+                item: 'taco seasoning'
+        },
+                      {
+                amount: '',
+                type: 'optional',
+                measurement: 'topping',
+                item: 'dried parsley (optional)'
+        }
+        ],
+        instructions: [
+            "Preheat oven to 350f degrees.",
+            "Spray a 9x13 baking dish with nonstick cooking spray.",
+            "Cook pasta according to package directions (remember, you are only using half a box of angel hair pasta here.)",
+            "Once pasta is cooked, drain well.",
+            "Place pasta back into the pot and add diced chicken, cream of chicken soups, salsa, sour cream, 1 cup of Mexican cheese blend and taco seasoning.",
+            "Stir well to combine (this will take a few minutes.)",
+            "Pour combined mixture into your prepared baking dish.",
+            "Top with remaining cheese and a sprinkling of dried parsley.",
+            "Cover with nonstick aluminum foil.",
+            "Cook in the oven for about 25 minutes (until hot and bubbly.)"
+
         ]
     }
     ]
-
